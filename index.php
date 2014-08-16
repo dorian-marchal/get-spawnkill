1f01642722b11fefd4e37e665a88818b8b31183b
Dorian <dorian@dorian-desktop.(none)>
2014-08-16 16:18:12 +0200

    - Force cache reload

--- index.php
+++ index.php
@@ -23,12 +23,12 @@
 	<meta charset="utf-8">
 	<title>JVC SpawnKill</title>
 	<link rel="stylesheet" href="bootstrap/css/bootstrap.min.css">
-	<link rel="stylesheet" href="css/style.css">
+	<link rel="stylesheet" href="css/style.css?1">
 	<link rel="stylesheet" href="fancybox/jquery.fancybox.css">
 	<script src="js/jquery.js"></script>
 	<script src="bootstrap/js/bootstrap.min.js"></script>
 	<script src="fancybox/jquery.fancybox.js"></script>
-	<script src="js/script.js"></script>
+	<script src="js/script.js?1"></script>
 </head>
 
 <body>
