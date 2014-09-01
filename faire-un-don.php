3eb176bb7d47e2ea0b5e791918d3625baea2f373
dorian-marchal <marchal.dorian@gmail.com>
2014-09-01 23:26:14 +0200

    Ajout d'un bouton de don

---
+++ faire-un-don.php
@@ -0,0 +1,22 @@
+<?php $page = "don"; ?>
+
+<?php include "header.php"; ?>
+<?php include "nav.php"; ?>
+<?php include "fixed-box.php"; ?>
+
+	<section class="col-lg-7 col-md-8">
+		<h1>Merci pour ton soutien !</h1>
+		<div id="donation" >
+			<form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top">
+			<input type="hidden" name="cmd" value="_s-xclick">
+			<input type="hidden" name="encrypted" value="-----BEGIN PKCS7-----MIIHLwYJKoZIhvcNAQcEoIIHIDCCBxwCAQExggEwMIIBLAIBADCBlDCBjjELMAkGA1UEBhMCVVMxCzAJBgNVBAgTAkNBMRYwFAYDVQQHEw1Nb3VudGFpbiBWaWV3MRQwEgYDVQQKEwtQYXlQYWwgSW5jLjETMBEGA1UECxQKbGl2ZV9jZXJ0czERMA8GA1UEAxQIbGl2ZV9hcGkxHDAaBgkqhkiG9w0BCQEWDXJlQHBheXBhbC5jb20CAQAwDQYJKoZIhvcNAQEBBQAEgYAZPXcZ7MbJIWD4FKLnRBRAqSesgxSRX8LHdqgmJBbzFsYNYsywBV9aIBLFsjEHqzCV8tCmdLLwXbFRdHeofezL5BJ+Y6OFH3Vu0JFL05u+ljMow8d4eeg5KpII4kn/I1cIx5KFV8Gs4j1UWaxNXoxLSeF0ERl1mQDXzax7aZO2gzELMAkGBSsOAwIaBQAwgawGCSqGSIb3DQEHATAUBggqhkiG9w0DBwQIzywCtYcTeh6AgYgfhi/nSJ48sVfwWXSvlsN2wLVb4ZTCJ+ymfWne6DcC6dz0JnA35QfuEWMV5zjii1PTl+0RwyyhaCoygoFpqS/kPGwRhX/D8C2Z1j8Ho5zr+KoMjj0b3XMDAq6mJumYzpmHcLvKikLzQSL9Hhl/YWE9lm72NwNe7MLNBB6npKJc/YbIp5DkZhkKoIIDhzCCA4MwggLsoAMCAQICAQAwDQYJKoZIhvcNAQEFBQAwgY4xCzAJBgNVBAYTAlVTMQswCQYDVQQIEwJDQTEWMBQGA1UEBxMNTW91bnRhaW4gVmlldzEUMBIGA1UEChMLUGF5UGFsIEluYy4xEzARBgNVBAsUCmxpdmVfY2VydHMxETAPBgNVBAMUCGxpdmVfYXBpMRwwGgYJKoZIhvcNAQkBFg1yZUBwYXlwYWwuY29tMB4XDTA0MDIxMzEwMTMxNVoXDTM1MDIxMzEwMTMxNVowgY4xCzAJBgNVBAYTAlVTMQswCQYDVQQIEwJDQTEWMBQGA1UEBxMNTW91bnRhaW4gVmlldzEUMBIGA1UEChMLUGF5UGFsIEluYy4xEzARBgNVBAsUCmxpdmVfY2VydHMxETAPBgNVBAMUCGxpdmVfYXBpMRwwGgYJKoZIhvcNAQkBFg1yZUBwYXlwYWwuY29tMIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQDBR07d/ETMS1ycjtkpkvjXZe9k+6CieLuLsPumsJ7QC1odNz3sJiCbs2wC0nLE0uLGaEtXynIgRqIddYCHx88pb5HTXv4SZeuv0Rqq4+axW9PLAAATU8w04qqjaSXgbGLP3NmohqM6bV9kZZwZLR/klDaQGo1u9uDb9lr4Yn+rBQIDAQABo4HuMIHrMB0GA1UdDgQWBBSWn3y7xm8XvVk/UtcKG+wQ1mSUazCBuwYDVR0jBIGzMIGwgBSWn3y7xm8XvVk/UtcKG+wQ1mSUa6GBlKSBkTCBjjELMAkGA1UEBhMCVVMxCzAJBgNVBAgTAkNBMRYwFAYDVQQHEw1Nb3VudGFpbiBWaWV3MRQwEgYDVQQKEwtQYXlQYWwgSW5jLjETMBEGA1UECxQKbGl2ZV9jZXJ0czERMA8GA1UEAxQIbGl2ZV9hcGkxHDAaBgkqhkiG9w0BCQEWDXJlQHBheXBhbC5jb22CAQAwDAYDVR0TBAUwAwEB/zANBgkqhkiG9w0BAQUFAAOBgQCBXzpWmoBa5e9fo6ujionW1hUhPkOBakTr3YCDjbYfvJEiv/2P+IobhOGJr85+XHhN0v4gUkEDI8r2/rNk1m0GA8HKddvTjyGw/XqXa+LSTlDYkqI8OwR8GEYj4efEtcRpRYBxV8KxAW93YDWzFGvruKnnLbDAF6VR5w/cCMn5hzGCAZowggGWAgEBMIGUMIGOMQswCQYDVQQGEwJVUzELMAkGA1UECBMCQ0ExFjAUBgNVBAcTDU1vdW50YWluIFZpZXcxFDASBgNVBAoTC1BheVBhbCBJbmMuMRMwEQYDVQQLFApsaXZlX2NlcnRzMREwDwYDVQQDFAhsaXZlX2FwaTEcMBoGCSqGSIb3DQEJARYNcmVAcGF5cGFsLmNvbQIBADAJBgUrDgMCGgUAoF0wGAYJKoZIhvcNAQkDMQsGCSqGSIb3DQEHATAcBgkqhkiG9w0BCQUxDxcNMTQwOTAxMjEyMTQ2WjAjBgkqhkiG9w0BCQQxFgQUaKKVfxe9A8Jadbft6YJ27YqvlAMwDQYJKoZIhvcNAQEBBQAEgYBTe7/YG0LKoqASDtHoeufhaZNcdOHIUtp/ePlsoqBbIysewu3PkHryVu2+aMnKNhRK6ID0n9VsIr8lpVZ2DPLUqK6qRKnykIhx2yaeM0mUD/FqaMD4NCxc/de1lfxwFzL9IjHU4s3i7I09exNl2VBzMvRLpkpQNZez4MmT1dgpuw==-----END PKCS7-----
+			">
+			<input type="image" src="https://www.paypalobjects.com/fr_FR/FR/i/btn/btn_donate_LG.gif" border="0" name="submit" alt="PayPal - la solution de paiement en ligne la plus simple et la plus sécurisée !">
+			<img alt="" border="0" src="https://www.paypalobjects.com/fr_FR/i/scr/pixel.gif" width="1" height="1">
+			</form>
+
+		</div>
+	</section>
+</div>
+
+<?php include "footer.php"; ?>
