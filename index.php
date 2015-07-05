985922b42ae124485f5897bc7017ecb5cdcf1efd
dorian-marchal <marchal.dorian@gmail.com>
2015-07-05 18:09:08 +0200

    Remplace les tabulations par des espaces

--- index.php
+++ index.php
@@ -3,228 +3,228 @@
 <?php include "nav.php"; ?>
 <?php include "fixed-box.php"; ?>
 
-	<section class="col-lg-7 col-md-8">
-		<h1>Les fonctionnalités</h1>
+    <section class="col-lg-7 col-md-8">
+        <h1>Les fonctionnalités</h1>
 
-		<div class="panel panel-default feature" >
-			<div class="panel-heading" >
-				<h3>Tout est configurable !</h3>
-			</div>
-			<div class="panel-body" >
-				<p>
-					SpawnKill est entiérement personnalisable : une fonctionnalité ne vous plait pas ?
-					Il suffit d'ouvrir les paramètres pour le désactiver.
-					<br>
-					En plus de ça, tous les modules ont leurs propres options.
-				</p>
-				<p>
-					Le panneau s'ouvre à la première installation ou en cliquant sur l'engrenage en haut à droite du forum
-				</p>
-				<br />
-				<div data-toggle="tooltip" class="feature-img" title="Panneau de configuration" >
-					<img src="img/settings.png" />
-				</div>
-			</div>
-		</div>
+        <div class="panel panel-default feature" >
+            <div class="panel-heading" >
+                <h3>Tout est configurable !</h3>
+            </div>
+            <div class="panel-body" >
+                <p>
+                    SpawnKill est entiérement personnalisable : une fonctionnalité ne vous plait pas ?
+                    Il suffit d'ouvrir les paramètres pour le désactiver.
+                    <br>
+                    En plus de ça, tous les modules ont leurs propres options.
+                </p>
+                <p>
+                    Le panneau s'ouvre à la première installation ou en cliquant sur l'engrenage en haut à droite du forum
+                </p>
+                <br />
+                <div data-toggle="tooltip" class="feature-img" title="Panneau de configuration" >
+                    <img src="img/settings.png" />
+                </div>
+            </div>
+        </div>
 
-		<div class="panel panel-default feature" >
-			<div class="panel-heading" >
-				<h3>Informations et actions sur les posts</h3>
-			</div>
-			<div class="panel-body" >
-				<p>
-					Affiche le sexe de l'auteur s'il est disponible ainsi que des boutons utiles,
-					comme l'affichage des topics de l'auteur ou la copie du lien permanent du post.
-				</p>
-				<br />
-				<div data-toggle="tooltip" class="feature-img" title="Infos et actions" >
-					<img src="img/infos.png" />
-				</div>
-				<br />
-				<br />
-				<br />
-				<p>
-					Ce module permet aussi d'afficher le profil d'un auteur dans une fenêtre modale.
-				</p>
-				<br />
-				<div data-toggle="tooltip" class="feature-img" title="Profil dans une modale" >
-					<img src="img/cdv.png" />
-				</div>
-			</div>
-		</div>
+        <div class="panel panel-default feature" >
+            <div class="panel-heading" >
+                <h3>Informations et actions sur les posts</h3>
+            </div>
+            <div class="panel-body" >
+                <p>
+                    Affiche le sexe de l'auteur s'il est disponible ainsi que des boutons utiles,
+                    comme l'affichage des topics de l'auteur ou la copie du lien permanent du post.
+                </p>
+                <br />
+                <div data-toggle="tooltip" class="feature-img" title="Infos et actions" >
+                    <img src="img/infos.png" />
+                </div>
+                <br />
+                <br />
+                <br />
+                <p>
+                    Ce module permet aussi d'afficher le profil d'un auteur dans une fenêtre modale.
+                </p>
+                <br />
+                <div data-toggle="tooltip" class="feature-img" title="Profil dans une modale" >
+                    <img src="img/cdv.png" />
+                </div>
+            </div>
+        </div>
 
-		<div class="panel panel-default feature" >
-			<div class="panel-heading" >
-				<h3>Citations</h3>
-			</div>
-			<div class="panel-body" >
-				<p>
-					Permet de citer seulement une partie d'un post en sélectionnant le texte à citer.
-				</p>
-				<br />
-				<div data-toggle="tooltip" class="feature-img" title="Citation partielle" >
-					<img src="img/citation-partielle.png" />
-				</div>
-			</div>
-		</div>
+        <div class="panel panel-default feature" >
+            <div class="panel-heading" >
+                <h3>Citations</h3>
+            </div>
+            <div class="panel-body" >
+                <p>
+                    Permet de citer seulement une partie d'un post en sélectionnant le texte à citer.
+                </p>
+                <br />
+                <div data-toggle="tooltip" class="feature-img" title="Citation partielle" >
+                    <img src="img/citation-partielle.png" />
+                </div>
+            </div>
+        </div>
 
-		<div class="panel panel-default feature" >
-			<div class="panel-heading" >
-				<h3>Intégration de contenus</h3>
-				<h4>(Contributeur : <a target="_blank" href="https://github.com/Apoxx" >Apoxx</a>)</h4>
-			</div>
-			<div class="panel-body" >
-				<p>
-					Remplace les liens vers du contenu par le contenu lui-même. Un bouton permet de cacher les contenus affichés<br>
-					Contenu pris en compte par le module : Youtube, DailyMotion, Vimeo, Pixule, Sondage.io, Twitter, Vocaroo, Images PNG/JPG/GIF.
-				</p>
-				<div data-toggle="tooltip" class="feature-img" title="Bouton de masquage des contenus" >
-					<img src="img/media-buttons.png" />
-				</div>
-				<br>
-				<br>
-				<div data-toggle="tooltip" class="feature-img" title="Intégration d'une vidéo Vimeo" >
-					<img src="img/vimeo.png" />
-				</div>
-			</div>
-		</div>
+        <div class="panel panel-default feature" >
+            <div class="panel-heading" >
+                <h3>Intégration de contenus</h3>
+                <h4>(Contributeur : <a target="_blank" href="https://github.com/Apoxx" >Apoxx</a>)</h4>
+            </div>
+            <div class="panel-body" >
+                <p>
+                    Remplace les liens vers du contenu par le contenu lui-même. Un bouton permet de cacher les contenus affichés<br>
+                    Contenu pris en compte par le module : Youtube, DailyMotion, Vimeo, Pixule, Sondage.io, Twitter, Vocaroo, Images PNG/JPG/GIF.
+                </p>
+                <div data-toggle="tooltip" class="feature-img" title="Bouton de masquage des contenus" >
+                    <img src="img/media-buttons.png" />
+                </div>
+                <br>
+                <br>
+                <div data-toggle="tooltip" class="feature-img" title="Intégration d'une vidéo Vimeo" >
+                    <img src="img/vimeo.png" />
+                </div>
+            </div>
+        </div>
 
-		<div class="panel panel-default feature" >
-			<div class="panel-heading" >
-				<h3>Être prévenu des nouveaux posts</h3>
-			</div>
-			<div class="panel-body" >
-				<p>
-					Ce module change le favicon de l'onglet pour indiquer combien de posts ont été ajoutés au topic depuis le chargement de la page.
-					Quand un messages est posté, un petit son est joué. Ce son peut évidemment être désactivé.
-				</p>
-				<div data-toggle="tooltip" class="feature-img" title="Nouveaux posts" >
-					<img src="img/nouveaux-posts.png" />
-				</div>
-			</div>
-		</div>
+        <div class="panel panel-default feature" >
+            <div class="panel-heading" >
+                <h3>Être prévenu des nouveaux posts</h3>
+            </div>
+            <div class="panel-body" >
+                <p>
+                    Ce module change le favicon de l'onglet pour indiquer combien de posts ont été ajoutés au topic depuis le chargement de la page.
+                    Quand un messages est posté, un petit son est joué. Ce son peut évidemment être désactivé.
+                </p>
+                <div data-toggle="tooltip" class="feature-img" title="Nouveaux posts" >
+                    <img src="img/nouveaux-posts.png" />
+                </div>
+            </div>
+        </div>
 
-		<div class="panel panel-default feature" >
-			<div class="panel-heading" >
-				<h3>Mise en avant des nouveaux topics</h3>
-			</div>
-			<div class="panel-body" >
-				<p>
-					Permet de voir facilement les nouveaux topics dans la liste des sujets
-				</p>
-				<div data-toggle="tooltip" class="feature-img" title="Nouveau topic" >
-					<img src="img/nouveau-topic.png" />
-				</div>
-			</div>
-		</div>
+        <div class="panel panel-default feature" >
+            <div class="panel-heading" >
+                <h3>Mise en avant des nouveaux topics</h3>
+            </div>
+            <div class="panel-body" >
+                <p>
+                    Permet de voir facilement les nouveaux topics dans la liste des sujets
+                </p>
+                <div data-toggle="tooltip" class="feature-img" title="Nouveau topic" >
+                    <img src="img/nouveau-topic.png" />
+                </div>
+            </div>
+        </div>
 
-		<div class="panel panel-default feature" >
-			<div class="panel-heading" >
-				<h3>Mise en avant des PEMT</h3>
-				<h4>(Contributeur : <a target="_blank" href="https://github.com/nounoursheureux" >nounoursheureux</a>)</h4>
-			</div>
-			<div class="panel-body" >
-				<p>
-					Met en avant les posts simultanés en surlignant l'heure de ces posts.
-				</p>
-				<div data-toggle="tooltip" class="feature-img" title="PEMT" >
-					<img src="img/pemt.png" />
-				</div>
-			</div>
-		</div>
+        <div class="panel panel-default feature" >
+            <div class="panel-heading" >
+                <h3>Mise en avant des PEMT</h3>
+                <h4>(Contributeur : <a target="_blank" href="https://github.com/nounoursheureux" >nounoursheureux</a>)</h4>
+            </div>
+            <div class="panel-body" >
+                <p>
+                    Met en avant les posts simultanés en surlignant l'heure de ces posts.
+                </p>
+                <div data-toggle="tooltip" class="feature-img" title="PEMT" >
+                    <img src="img/pemt.png" />
+                </div>
+            </div>
+        </div>
 
-		<div class="panel panel-default feature" >
-			<div class="panel-heading" >
-				<h3>Mise en avant des posts de l'auteur</h3>
-				<h4>(Contributeur : <a target="_blank" href="https://github.com/delgan" >Delgan</a>)</h4>
-			</div>
-			<div class="panel-body" >
-				<p>
-					Ce module ajoute une petite couronne devant les posts de l'auteur pour l'identifier facilement.
-				</p>
-				<div data-toggle="tooltip" class="feature-img" title="A Crown for a King !" >
-					<img src="img/crown.png" />
-				</div>
-			</div>
-		</div>
+        <div class="panel panel-default feature" >
+            <div class="panel-heading" >
+                <h3>Mise en avant des posts de l'auteur</h3>
+                <h4>(Contributeur : <a target="_blank" href="https://github.com/delgan" >Delgan</a>)</h4>
+            </div>
+            <div class="panel-body" >
+                <p>
+                    Ce module ajoute une petite couronne devant les posts de l'auteur pour l'identifier facilement.
+                </p>
+                <div data-toggle="tooltip" class="feature-img" title="A Crown for a King !" >
+                    <img src="img/crown.png" />
+                </div>
+            </div>
+        </div>
 
-		<div class="panel panel-default feature" >
-			<div class="panel-heading" >
-				<h3>Mise en valeur de vos posts</h3>
-				<h4>(Contributeur : <a target="_blank" href="https://github.com/nounoursheureux" >nounoursheureux</a>)</h4>
-			</div>
-			<div class="panel-body" >
-				<p>
-					Ce module colore votre pseudo de la couleur principale du module pour retrouver facilement vos posts.
-				</p>
-				<div data-toggle="tooltip" class="feature-img" title="Mise en avant de vos posts" >
-					<img src="img/hilight.png" />
-				</div>
-			</div>
-		</div>
+        <div class="panel panel-default feature" >
+            <div class="panel-heading" >
+                <h3>Mise en valeur de vos posts</h3>
+                <h4>(Contributeur : <a target="_blank" href="https://github.com/nounoursheureux" >nounoursheureux</a>)</h4>
+            </div>
+            <div class="panel-body" >
+                <p>
+                    Ce module colore votre pseudo de la couleur principale du module pour retrouver facilement vos posts.
+                </p>
+                <div data-toggle="tooltip" class="feature-img" title="Mise en avant de vos posts" >
+                    <img src="img/hilight.png" />
+                </div>
+            </div>
+        </div>
 
-		<div class="panel panel-default feature" >
-			<div class="panel-heading" >
-				<h3>Masquer les posts d'un membre</h3>
-			</div>
-			<div class="panel-body" >
-				<p>
-					Ce module permet de masquer les posts d'un certain auteur.
-				</p>
-				<div data-toggle="tooltip" class="feature-img" title="Bouton de masquage" >
-					<img src="img/block-button.png" />
-				</div>
-				<br />
-				<br />
-				<p>
-					Par défaut, il est possible de réafficher les posts masqués mais
-					une option est disponible pour les supprimer totalement de la page.
-				</p>
-				<div data-toggle="tooltip" class="feature-img" title="Post ignoré" >
-					<img src="img/blocked-post.png" />
-				</div>
-			</div>
-		</div>
+        <div class="panel panel-default feature" >
+            <div class="panel-heading" >
+                <h3>Masquer les posts d'un membre</h3>
+            </div>
+            <div class="panel-body" >
+                <p>
+                    Ce module permet de masquer les posts d'un certain auteur.
+                </p>
+                <div data-toggle="tooltip" class="feature-img" title="Bouton de masquage" >
+                    <img src="img/block-button.png" />
+                </div>
+                <br />
+                <br />
+                <p>
+                    Par défaut, il est possible de réafficher les posts masqués mais
+                    une option est disponible pour les supprimer totalement de la page.
+                </p>
+                <div data-toggle="tooltip" class="feature-img" title="Post ignoré" >
+                    <img src="img/blocked-post.png" />
+                </div>
+            </div>
+        </div>
 
-		<div class="panel panel-default feature" >
-			<div class="panel-heading" >
-				<h3>Accès rapide à la dernière page d'un topic</h3>
-			</div>
-			<div class="panel-body" >
-				<p>
-					Ce module ajoute un lien sur les icônes des topics dans la liste des sujets permettant d'accéder directement à la dernière page de ce topic.
-				</p>
-				<div data-toggle="tooltip" class="feature-img" title="Accès à la dernière page" >
-					<img src="img/last-page.png" />
-				</div>
-				<br />
-				<br />
-				<p>
-					De plus, il ajoute un lien en haut à gauche du titre du topic qui peut être glissé/déposé dans vos favoris pour en faire un raccourci
-					qui pointera <strong>toujours</strong> vers la dernière page du topic.
-				</p>
-				<div data-toggle="tooltip" class="feature-img" title="Raccourci vers la dernière page" >
-					<img src="img/last-page-bookmark.png" />
-				</div>
-			</div>
-		</div>
+        <div class="panel panel-default feature" >
+            <div class="panel-heading" >
+                <h3>Accès rapide à la dernière page d'un topic</h3>
+            </div>
+            <div class="panel-body" >
+                <p>
+                    Ce module ajoute un lien sur les icônes des topics dans la liste des sujets permettant d'accéder directement à la dernière page de ce topic.
+                </p>
+                <div data-toggle="tooltip" class="feature-img" title="Accès à la dernière page" >
+                    <img src="img/last-page.png" />
+                </div>
+                <br />
+                <br />
+                <p>
+                    De plus, il ajoute un lien en haut à gauche du titre du topic qui peut être glissé/déposé dans vos favoris pour en faire un raccourci
+                    qui pointera <strong>toujours</strong> vers la dernière page du topic.
+                </p>
+                <div data-toggle="tooltip" class="feature-img" title="Raccourci vers la dernière page" >
+                    <img src="img/last-page-bookmark.png" />
+                </div>
+            </div>
+        </div>
 
-		<div class="panel panel-default feature" >
-			<div class="panel-heading" >
-				<h3>Raccourcis clavier</h3>
-				<h4>(Contributeur : <a target="_blank" href="https://github.com/superdrac" >Superdrac</a>)</h4>
-			</div>
-			<div class="panel-body" >
-				<p>
-					Ce module ajoute des raccourcis clavier pour effectuer des actions comme changer de page ou revenir à la liste des sujets.
-				</p>
-				<div data-toggle="tooltip" class="feature-img" title="Raccourcis clavier" >
-					<img src="img/keys.png" />
-				</div>
-			</div>
-		</div>
+        <div class="panel panel-default feature" >
+            <div class="panel-heading" >
+                <h3>Raccourcis clavier</h3>
+                <h4>(Contributeur : <a target="_blank" href="https://github.com/superdrac" >Superdrac</a>)</h4>
+            </div>
+            <div class="panel-body" >
+                <p>
+                    Ce module ajoute des raccourcis clavier pour effectuer des actions comme changer de page ou revenir à la liste des sujets.
+                </p>
+                <div data-toggle="tooltip" class="feature-img" title="Raccourcis clavier" >
+                    <img src="img/keys.png" />
+                </div>
+            </div>
+        </div>
 
-	</section>
+    </section>
 </div>
 
 <?php include "footer.php"; ?>
